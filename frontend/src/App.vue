b161fb75540c7cc43a46f562582767a74222d50e
tobiaswuerth <21124705+tobiaswuerth@users.noreply.github.com>
2025-05-16 12:58:21 +0200

    icon to svg

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,8 +7,8 @@ import ErrorOverlay from './components/ErrorOverlay.vue'
   <ErrorOverlay />
   <div class="m-5 p-5 rounded-lg" style="background-color: var(--p-stone-50);">
     <div class="flex items-center mb-4 pb-3" style="border-bottom: 1px solid var(--p-stone-200);">
-      <img src="/icon.png" alt="Logo" height="50" class="mr-0" />
-      <h1 class="text-4xl font-bold text-gray-800 m-0">emantic Index</h1>
+      <img src="/icon.svg" alt="Logo" height="50" class="mr-0 pr-0" />
+      <h1 class="text-4xl font-bold text-gray-800 m-0 pl-0">emantic Index</h1>
     </div>
     <Home />
   </div>
